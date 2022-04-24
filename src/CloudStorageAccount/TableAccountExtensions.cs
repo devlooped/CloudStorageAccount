20e835587c06ecee362be0282abafb60553112fc
Daniel Cazzulino <daniel@cazzulino.com>
2022-04-24 04:29:19 -0300

    Port tests, add client extension methods

---
+++ src/CloudStorageAccount/TableAccountExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Azure;
+using Azure.Data.Tables;
+
+namespace Devlooped;
+
+/// <summary>
+/// Provides the <see cref="CreateCloudTableClient(CloudStorageAccount)"/> extension method.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class TableAccountExtensions
+{
+    /// <summary>
+    /// Creates a Table service client from the given account.
+    /// </summary>
+    public static TableServiceClient CreateCloudTableClient(this CloudStorageAccount account)
+    {
+        if (account.TableEndpoint == null)
+            throw new InvalidOperationException("No table endpoint configured.");
+
+        if (account.Credentials.IsSAS)
+            return new TableServiceClient(account.TableEndpoint, new AzureSasCredential(account.Credentials.Signature!));
+        else if (account.Credentials.IsSharedKey)
+            return new TableServiceClient(account.TableEndpoint,
+                new TableSharedKeyCredential(account.Credentials.AccountName, account.Credentials.AccountKey));
+        else if (account.Credentials.IsAnonymous)
+            return new TableServiceClient(account.TableEndpoint);
+
+        throw new InvalidOperationException("Account credentials are not supported for Table client.");
+    }
+}
