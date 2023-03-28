533c6b5dc0299e60b6d359015bf580a71c814c9d
Daniel Cazzulino <daniel@cazzulino.com>
2023-03-28 16:49:14 -0300

    Add SponsorLink to ensure ongoing development and support

---
+++ src/CloudStorageAccount.CodeAnalysis/SponsorLinker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Devlooped;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CloudStorageAccount.CodeAnalysis;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "Devlooped.CloudStorageAccount",
+        diagnosticsIdPrefix: "DCSA",
+        version: new Version(ThisAssembly.Info.Version).ToString(3)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
